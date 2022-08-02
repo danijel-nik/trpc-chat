50cf8c46b1bc2bf14f361139d56ddff87bced3b0
danijel_nik <danijel.nikolic@ymail.com>
2022-08-02 22:09:00 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+- This project (api-server and client) configured by `npx create-mf-app`.
+
+- For parallel starts and dependencies installs, use package.json in root of this project - for workspaces the project must be private.
+
+- For typescript configuration file, go to root dir (api-server or client) and type `npc tsc --init` so file `tsconfig.json` will be generated.
---
+++ packages/api-server/index.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import * as trpc from "@trpc/server";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import cors from "cors";
+
+const appRouter = trpc.router()
+	.query("hello", {
+		resolve() {
+			return "Hello world!!"
+		},
+	});
+
+export type AppRouter = typeof appRouter;
+
+const app = express();
+app.use(cors());
+const port = 5000;
+
+app.use("/trpc", trpcExpress.createExpressMiddleware({
+	router: appRouter,
+	createContext: () => null
+}));
+
+app.get("/", (req, res) => {
+	res.send("Hello from api-server");
+});
+
+app.listen(port, () => {
+	console.log(`api-server listening at http://localhost:${port}`);
+});
